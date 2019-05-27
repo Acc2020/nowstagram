56edb45d10cb763b00f6362964c573f73e2da8af
laborbird <18226568070@139.com>
2019-05-27 14:21:32 +0800

    Add image

--- nowstagram/models.py
+++ nowstagram/models.py
@@ -1,13 +1,30 @@
 # -*- encoding = UTF-8 -*-
 
 from nowstagram import db
+from datetime import datetime
 import random
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True )
+    url = db.Column(db.String(512))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_id = db.Column(db.DateTime)
+
+    def __init__(self, user_id, url):
+        self.url = url
+        self.user_id = user_id
+        self.created_id = datetime.now()
+
+    def __repr__(self):
+        return '<Image %d %s>' % (self.id, self.url)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(32))
     head_url = db.Column(db.String(256))
+    images = db.relationship('Image')
 
     def __init__(self, username, password):
         self.username = username
