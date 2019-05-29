389ef7f1c00b2eb546eafbb7b50f7a4e88e73354
laborbird <18226568070@139.com>
2019-05-29 23:12:25 +0800

    Add login.html

--- nowstagram/views.py
+++ nowstagram/views.py
@@ -2,7 +2,7 @@
 
 from nowstagram import app
 from nowstagram.models import User, Image
-from flask import render_template, redirect
+from flask import render_template, redirect, request, flash
 
 
 @app.route('/')
@@ -24,4 +24,26 @@ def profile(user_id):
     user = User.query.get(user_id)
     if user == None:
         return redirect('/')
-    return render_template('profile.html', user = user)
+    return render_template('profile.html', user=user)
+
+
+@app.route('/regloginpage/')
+def regloginpage():
+    return render_template('login.html')
+
+
+def redirect_with_msg(target, msg, category):
+    if msg != None:
+        flash(msg,category=category)
+    return redirect(target)
+
+@app.route('/reg/')
+def reg():
+    # request.args requser.form
+    username = request.values.get('username').strip()
+    password = request.values.get('username').strip()
+
+    user = User.query.filter_by(username=username).first()
+    if user != None:
+        redirect_with_msg(u'用户名已经存在','relogin')
+        return redirect('/regloginpage/')
