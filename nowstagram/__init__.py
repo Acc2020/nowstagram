6ad10cfc66f9a93dd1a9a19f052327942ac7d324
laborbird <18226568070@139.com>
2019-06-29 18:56:18 +0800

    Finish denglu

--- nowstagram/__init__.py
+++ nowstagram/__init__.py
@@ -7,15 +7,7 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 
-app.config['MAIL_DEBUG'] = True             # 开启debug，便于调试看信息
-app.config['MAIL_SUPPRESS_SEND'] = False    # 发送邮件，为True则不发送
-app.config['MAIL_SERVER'] = 'smtp.qq.com'   # 邮箱服务器
-app.config['MAIL_PORT'] = 465               # 端口
-app.config['MAIL_USE_SSL'] = True           # 重要，qq邮箱需要使用SSL
-app.config['MAIL_USE_TLS'] = False          # 不需要使用TLS
-app.config['MAIL_USERNAME'] = '807098855@qq.com'  # 填邮箱
-app.config['MAIL_PASSWORD'] = 'ovqpuxwmtsvrbgaa'      # 填授权码
-app.config['MAIL_DEFAULT_SENDER'] ='807098855@qq.com'  # 填邮箱，默认发送者
+
 
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 app.config.from_pyfile('app.conf')
